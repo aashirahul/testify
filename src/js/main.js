5f2a01f4f72b184b80ec9d3150ed02e6c73582da
Ashima Shrivastava <ashima206@gmail.com>
2016-11-03 11:57:39 -0400

    test one passed

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,14 @@
-function fizz () {
+function fizz (number) {
+var num = number;
+return num;
   // TODO
-  var count;
-  for(count=0; count<=100; count++){
-  	if(count%3!==0&&count%5!==0){
-  		console.log(count);
-  	}
-  }
+
 
 };
 
 function fizzBuzz (start, end) {
   // Write fizzBuzz here once fizz is finished.
+  // loop, fizz. console
 };
 
 export { fizz };
