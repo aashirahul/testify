2457429e91ceb229cfc56008b741c5a6b52a320c
Ashima Shrivastava <ashima206@gmail.com>
2016-11-03 18:48:13 -0400

    refactored for cleaner code

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,15 @@
 function fizz (number) {
-if(number%3==0&& number%5==0){
-	var num = 'fizzbuzz';
-} else if (number%3==0){
-	var num = 'fizz';
-}else if (number%5==0){
-	var num = 'buzz';
-}else {
-var num = number;
-}
-return num;
-  // TODO
-
-
+	var num;
+	if(number%3==0&& number%5==0){
+		num = 'fizzbuzz';
+	} else if (number%3==0){
+		num = 'fizz';
+	}else if (number%5==0){
+		num = 'buzz';
+	}else {
+		num = number;
+	}
+	return num;
 };
 
 function fizzBuzz (start, end) {
