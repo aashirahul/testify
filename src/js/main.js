3cd508b7e72861fffc1a2a127cfa1801c63487da
Ashima Shrivastava <ashima206@gmail.com>
2016-11-03 11:48:27 -0400

    test failed

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 function fizz () {
   // TODO
+  var count;
+  for(count=0; count<=100; count++){
+  	if(count%3!==0&&count%5!==0){
+  		console.log(count);
+  	}
+  }
+
 };
 
 function fizzBuzz (start, end) {
