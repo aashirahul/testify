95daf57222775b5fd68831308ab22815fd04b91b
Ashima Shrivastava <ashima206@gmail.com>
2016-11-03 22:46:06 -0400

    added another function for backstage passes..tests are passing

--- src/js/hard.js
+++ src/js/hard.js
@@ -19,25 +19,12 @@ class GildedRose {
     }else {
       if (this.quality < 50) {
            this.quality = this.quality + 1;
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sellIn < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-          if (this.sellIn < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
+           this.checkingBackStagePasses();
         }
       }
-    }
   
  
         
-
-
     if (this.name != 'Sulfuras, Hand of Ragnaros') {
       this.sellIn = this.sellIn - 1;
     }
@@ -64,13 +51,30 @@ class GildedRose {
   }
 }
 
-qualityDecreasesWithTime(){
-      if(this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert'){
-        return true;
-    }else {
+  qualityDecreasesWithTime(){
+    if(this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert'){
+      return true;
+    } else {
       return false;
     }
   }
+
+  workingBackStagePasses(){
+    if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.sellIn < 11) {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+          }
+        }
+          if (this.sellIn < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+        }
+      }
+
+
 }
 
 
