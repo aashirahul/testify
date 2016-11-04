240bf2b4e3e2be5a6883f996ed9f0404bd482b4c
Ashima Shrivastava <ashima206@gmail.com>
2016-11-04 13:40:07 -0400

    added functions to refactor code

--- src/js/hard.js
+++ src/js/hard.js
@@ -6,39 +6,24 @@ class GildedRose {
   }
 
   tick () {
-    if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (this.quality > 0) {
-        if (this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.quality = this.quality - 1;
-        }
-      }
-    } else {
+    if(this.qualityDecreasesWithTime()){
+      if(this.quality>0){
+        this.decreasingQualityWithTime();
+      }  
+    }else {
       if (this.quality < 50) {
-        this.quality = this.quality + 1;
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sellIn < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-          if (this.sellIn < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
+           this.quality = this.quality + 1;
+           this.processingBackStagePasses();
         }
       }
-    }
-    if (this.name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
-    }
+         
+  this.decreaseInSellInWithTime();
+    
     if (this.sellIn < 0) {
       if (this.name != 'Aged Brie') {
         if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.quality > 0) {
-            if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              this.quality = this.quality - 1;
-            }
+            this.decreasingQualityWithTime();
           }
         } else {
           this.quality = this.quality - this.quality;
@@ -46,10 +31,54 @@ class GildedRose {
       } else {
         if (this.quality < 50) {
           this.quality = this.quality + 1;
-        }
+        
       }
     }
   }
 }
+
+  decreaseInSellInWithTime(){
+    if(this.name != 'Sulfuras, Hand of Ragnaros'){
+      this.sellIn = this.sellIn - 1;
+    }
+  }
+
+  decreasingQualityWithTime(){
+    if (this.name != 'Sulfuras, Hand of Ragnaros'){
+          if(this.name=='Conjured Mana Cake'){
+            this.quality = this.quality-2;
+               } else {
+                  this.quality=this.quality-1;
+                }
+              }
+
+  }
+
+  qualityDecreasesWithTime(){
+    if(this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert'){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  processingBackStagePasses(){
+    if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.sellIn < 11) {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+          }
+        }
+          if (this.sellIn < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+        }
+      }
+
+
+}
+
 
 export { GildedRose };
